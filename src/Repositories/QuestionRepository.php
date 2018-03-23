b71e6f5e1220606aaa6d2e94016536b58c182944
Shahiem Seymor <s.seymor@live.nl>
2018-03-23 19:05:22 +0100

    Add ability to update an answer and add new answers

--- src/Repositories/QuestionRepository.php
+++ src/Repositories/QuestionRepository.php
@@ -60,6 +60,33 @@ class QuestionRepository
         // foreach (array_filter($post['answers']) as $answer) {;
         // $poll->question = $data['question'];
         // $poll->save();
+        // foreach (array_filter($post['answers']) as $answer) {;
+        // $poll->question = $data['question'];
+        // $poll->save();
+        // echo count($poll->answers());
+
+        foreach ($poll->answers as $key => $answer) {
+            $postKey = $data['answers'][$key + 1];
+
+            if (isset($postKey)) {
+               $answer->update(array('answer' => $postKey)); // If the answer exists, update it!
+            } else {
+                // Remove answer
+            }
+        }
+
+        // Add new answers to database
+        foreach ($data as $key => $pollAnswers) {
+            foreach ($pollAnswers as $answerKey => $answer) {
+                if (!isset($poll->answers[$answerKey - 1])) {
+                    $newAnswers[] = new  \Reflar\Polls\Answer(['answer' => $answer]);
+                }
+            }
+        }
+
+        if (isset($newAnswers)) {
+            $poll->answers()->saveMany($newAnswers);
+        }
     }
 
     public function deletePoll($id)
