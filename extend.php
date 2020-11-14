6db5c570f72103bdd24e92386f2c2eb047756431
karaok <karaok491@gmail.com>
2020-11-14 21:22:12 +0000

    Use event extender

--- extend.php
+++ extend.php
@@ -21,18 +21,25 @@ return [
     (new Extend\Frontend('forum'))
         ->js(__DIR__.'/js/dist/forum.js')
         ->css(__DIR__.'/resources/less/forum.less'),
+
     (new Extend\Frontend('admin'))
         ->js(__DIR__.'/js/dist/admin.js')
         ->css(__DIR__.'/resources/less/admin.less'),
+
     new Extend\Locales(__DIR__.'/resources/locale'),
+
     (new Extend\Routes('api'))
         ->patch('/fof/polls/{id}', 'fof.polls.edit', Controllers\EditPollController::class)
         ->delete('/fof/polls/{id}', 'fof.polls.delete', Controllers\DeletePollController::class)
         ->patch('/fof/polls/{id}/vote', 'fof.polls.vote', Controllers\VotePollController::class),
+
     (new Extend\Model(Discussion::class))
         ->hasOne('poll', Poll::class, 'discussion_id', 'id'),
-    new Extend\Compat(function (Dispatcher $events) {
+
+    (new Extend\Event())
+        ->listen(Saving::class, Listeners\SavePollsToDatabase::class),
+
+    function (Dispatcher $events) {
         $events->subscribe(Listeners\AddDiscussionPollRelationship::class);
-        $events->listen(Saving::class, Listeners\SavePollsToDatabase::class);
-    }),
+    },
 ];
