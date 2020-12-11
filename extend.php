734ee74a2aa2939bb09c5f7eb2a32161af4a462a
karaok <karaok491@gmail.com>
2020-12-11 22:14:10 +0000

    beta 15 prep

--- extend.php
+++ extend.php
@@ -11,22 +11,26 @@
 
 namespace FoF\Polls;
 
+use Flarum\Api\Controller;
+use Flarum\Api\Serializer\DiscussionSerializer;
+use Flarum\Api\Serializer\UserSerializer;
 use Flarum\Discussion\Discussion;
 use Flarum\Discussion\Event\Saving;
 use Flarum\Extend;
+use Flarum\User\User;
 use FoF\Polls\Api\Controllers;
-use Illuminate\Events\Dispatcher;
+use FoF\Polls\Api\Serializers\PollSerializer;
 
 return [
     (new Extend\Frontend('forum'))
-        ->js(__DIR__.'/js/dist/forum.js')
-        ->css(__DIR__.'/resources/less/forum.less'),
+        ->js(__DIR__ . '/js/dist/forum.js')
+        ->css(__DIR__ . '/resources/less/forum.less'),
 
     (new Extend\Frontend('admin'))
-        ->js(__DIR__.'/js/dist/admin.js')
-        ->css(__DIR__.'/resources/less/admin.less'),
+        ->js(__DIR__ . '/js/dist/admin.js')
+        ->css(__DIR__ . '/resources/less/admin.less'),
 
-    new Extend\Locales(__DIR__.'/resources/locale'),
+    new Extend\Locales(__DIR__ . '/resources/locale'),
 
     (new Extend\Routes('api'))
         ->patch('/fof/polls/{id}', 'fof.polls.edit', Controllers\EditPollController::class)
@@ -39,7 +43,27 @@ return [
     (new Extend\Event())
         ->listen(Saving::class, Listeners\SavePollsToDatabase::class),
 
-    function (Dispatcher $events) {
-        $events->subscribe(Listeners\AddDiscussionPollRelationship::class);
-    },
+    (new Extend\ApiSerializer(DiscussionSerializer::class))
+        ->hasOne('poll', PollSerializer::class),
+
+    (new Extend\ApiSerializer(UserSerializer::class))
+        ->mutate(function (UserSerializer $serializer, User $user, array $attributes): array {
+            $attributes['canEditPolls'] = $serializer->getActor()->can('discussion.polls');
+            $attributes['canStartPolls'] = $serializer->getActor()->can('startPolls');
+            $attributes['canSelfEditPolls'] = $serializer->getActor()->can('selfEditPolls');
+            $attributes['canVotePolls'] = $serializer->getActor()->can('votePolls');
+            return $attributes;
+        }),
+
+    (new Extend\ApiController(Controller\ListDiscussionsController::class))
+        ->addInclude('poll'),
+
+    (new Extend\ApiController(Controller\ShowDiscussionController::class))
+        ->addInclude(['poll', 'poll.options', 'poll.votes', 'poll.votes.user', 'poll.votes.option']),
+
+    (new Extend\ApiController(Controller\CreateDiscussionController::class))
+        ->addInclude(['poll', 'poll.options', 'poll.votes', 'poll.votes.user', 'poll.votes.option']),
+
+    (new Extend\ApiController(Controller\UpdateDiscussionController::class))
+        ->addInclude(['poll', 'poll.options', 'poll.votes', 'poll.votes.user', 'poll.votes.option']),
 ];
--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,49 +1,38 @@
-import { extend } from 'flarum/extend';
-import PermissionGrid from 'flarum/components/PermissionGrid';
+import app from 'flarum/app';
 
 app.initializers.add('fof/polls', () => {
-    extend(PermissionGrid.prototype, 'moderateItems', (items) => {
-        items.add(
-            'fof-polls',
+    app.extensionData
+        .for('fof-polls')
+        .registerPermission(
             {
                 icon: 'fa fa-pencil-alt',
                 label: app.translator.trans('fof-polls.admin.permissions.moderate'),
                 permission: 'discussion.polls',
             },
-            95
-        );
-    });
-
-    extend(PermissionGrid.prototype, 'startItems', (items) => {
-        items.add(
-            'fof-polls-start',
+            'moderate'
+        )
+        .registerPermission(
             {
                 icon: 'fa fa-signal',
                 label: app.translator.trans('fof-polls.admin.permissions.start'),
                 permission: 'startPolls',
             },
-            95
-        );
-    });
-
-    extend(PermissionGrid.prototype, 'replyItems', (items) => {
-        items.add(
-            'fof-polls-edit',
+            'start'
+        )
+        .registerPermission(
             {
                 icon: 'fa fa-pencil-alt',
                 label: app.translator.trans('fof-polls.admin.permissions.self_edit'),
                 permission: 'selfEditPolls',
             },
-            70
-        );
-        items.add(
-            'fof-polls-vote',
+            'reply'
+        )
+        .registerPermission(
             {
                 icon: 'fa fa-signal',
                 label: app.translator.trans('fof-polls.admin.permissions.vote'),
                 permission: 'votePolls',
             },
-            80
+            'reply'
         );
-    });
 });
