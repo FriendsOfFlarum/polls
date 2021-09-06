039aad973af581610fae8575f13b0ee025536e1a
Rafał Całka <calka.rafal@gmail.com>
2021-09-06 14:13:06 +0200

    Use native HTML date-time picker

--- js/src/forum/components/CreatePollModal.js
+++ js/src/forum/components/CreatePollModal.js
@@ -3,179 +3,170 @@ import app from 'flarum/forum/app';
 import Button from 'flarum/common/components/Button';
 import Modal from 'flarum/common/components/Modal';
 import Switch from 'flarum/common/components/Switch';
-import classList from 'flarum/common/utils/classList';
 import Stream from 'flarum/common/utils/Stream';
-import flatpickr from 'flatpickr';
 
+/*global dayjs*/
 export default class CreatePollModal extends Modal {
-    oninit(vnode) {
-        super.oninit(vnode);
+  oninit(vnode) {
+    super.oninit(vnode);
 
-        this.options = [Stream(''), Stream('')];
+    this.options = [Stream(''), Stream('')];
 
-        this.question = Stream('');
+    this.question = Stream('');
 
-        this.endDate = Stream();
+    this.endDate = Stream();
 
-        this.publicPoll = Stream(false);
+    this.publicPoll = Stream(false);
 
-        if (this.attrs.poll && this.attrs.poll.relationships) {
-            const poll = this.attrs.poll;
+    this.dateTimeFormat = 'YYYY-MM-DDTHH:mm';
 
-            this.options = poll.relationships.options.map((o) => Stream(o));
-            this.question(poll.question);
-            this.endDate(!poll.endDate || isNaN(poll.endDate.getTime()) ? null : poll.endDate);
-            this.publicPoll(poll.publicPoll);
-        }
+    if (this.attrs.poll && this.attrs.poll.relationships) {
+      const poll = this.attrs.poll;
+
+      this.options = poll.relationships.options.map((o) => Stream(o));
+      this.question(poll.question);
+      this.endDate(!poll.endDate || isNaN(poll.endDate.getTime()) ? null : poll.endDate);
+      this.publicPoll(poll.publicPoll);
     }
+  }
 
-    title() {
-        return app.translator.trans('fof-polls.forum.modal.add_title');
-    }
+  title() {
+    return app.translator.trans('fof-polls.forum.modal.add_title');
+  }
 
-    className() {
-        return 'PollDiscussionModal Modal--small';
-    }
+  className() {
+    return 'PollDiscussionModal Modal--small';
+  }
 
-    configDatePicker(vnode) {
-        flatpickr(vnode.dom, {
-            enableTime: true,
-            minDate: this.endDate() || 'today',
-            dateFormat: 'Y-m-d H:i',
-            defaultDate: this.endDate(),
-            wrap: true,
+  content() {
+    console.log(this.endDate());
 
-            onChange: (dates) => this.endDate(dates[0]),
-        });
-    }
+    return [
+      <div className="Modal-body">
+        <div className="PollDiscussionModal-form">
+          <div className="Form-group">
+            <label className="label">{app.translator.trans('fof-polls.forum.modal.question_placeholder')}</label>
 
-    content() {
-        return [
-            <div className="Modal-body">
-                <div className="PollDiscussionModal-form">
-                    <div className="Form-group">
-                        <label className="label">{app.translator.trans('fof-polls.forum.modal.question_placeholder')}</label>
+            <input type="text" name="question" className="FormControl" bidi={this.question} />
+          </div>
 
-                        <input type="text" name="question" className="FormControl" bidi={this.question} />
-                    </div>
+          <div className="PollModal--answers Form-group">
+            <label className="label PollModal--answers-title">
+              <span>{app.translator.trans('fof-polls.forum.modal.options_label')}</span>
 
-                    <div className="PollModal--answers Form-group">
-                        <label className="label PollModal--answers-title">
-                            <span>{app.translator.trans('fof-polls.forum.modal.options_label')}</span>
+              {Button.component({
+                className: 'Button PollModal--button small',
+                icon: 'fas fa-plus',
+                onclick: this.addOption.bind(this),
+              })}
+            </label>
 
-                            {Button.component({
-                                className: 'Button PollModal--button small',
-                                icon: 'fas fa-plus',
-                                onclick: this.addOption.bind(this),
-                            })}
-                        </label>
+            {this.displayOptions()}
+          </div>
 
-                        {this.displayOptions()}
-                    </div>
+          <div className="Form-group">
+            <label className="label">{app.translator.trans('fof-polls.forum.modal.date_placeholder')}</label>
 
-                    <div className="Form-group">
-                        <label className="label">{app.translator.trans('fof-polls.forum.modal.date_placeholder')}</label>
-
-                        <div className="PollModal--date" oncreate={this.configDatePicker.bind(this)}>
-                            <input style="opacity: 1; color: inherit" className="FormControl" data-input />
-                            {Button.component({
-                                className: 'Button PollModal--button',
-                                icon: 'fas fa-times',
-                                'data-clear': true,
-                            })}
-                        </div>
-                    </div>
-
-                    <div className="Form-group">
-                        {Switch.component(
-                            {
-                                state: this.publicPoll() || false,
-                                onchange: this.publicPoll,
-                            },
-                            app.translator.trans('fof-polls.forum.modal.public_poll_label')
-                        )}
-                    </div>
-
-                    <div className="Form-group">
-                        {Button.component(
-                            {
-                                type: 'submit',
-                                className: 'Button Button--primary PollModal-SubmitButton',
-                                loading: this.loading,
-                            },
-                            app.translator.trans('fof-polls.forum.modal.submit')
-                        )}
-                    </div>
-                </div>
-            </div>,
-        ];
-    }
-
-    displayOptions() {
-        return Object.keys(this.options).map((el, i) => (
-            <div className="Form-group">
-                <fieldset className="Poll-answer-input">
-                    <input
-                        className="FormControl"
-                        type="text"
-                        name={'answer' + (i + 1)}
-                        bidi={this.options[i]}
-                        placeholder={app.translator.trans('fof-polls.forum.modal.option_placeholder') + ' #' + (i + 1)}
-                    />
-                </fieldset>
-                {i >= 2
-                    ? Button.component({
-                          type: 'button',
-                          className: 'Button Button--warning PollModal--button',
-                          icon: 'fas fa-minus',
-                          onclick: i >= 2 ? this.removeOption.bind(this, i) : '',
-                      })
-                    : ''}
+            <div className="PollModal--date">
+              <input type="datetime-local" className="FormControl" min={dayjs().format(this.dateTimeFormat)} bidi={this.endDate} />
+              {Button.component({
+                className: 'Button PollModal--button',
+                icon: 'fas fa-times',
+                onclick: () => this.endDate(null),
+              })}
             </div>
-        ));
+          </div>
+
+          <div className="Form-group">
+            {Switch.component(
+              {
+                state: this.publicPoll() || false,
+                onchange: this.publicPoll,
+              },
+              app.translator.trans('fof-polls.forum.modal.public_poll_label')
+            )}
+          </div>
+
+          <div className="Form-group">
+            {Button.component(
+              {
+                type: 'submit',
+                className: 'Button Button--primary PollModal-SubmitButton',
+                loading: this.loading,
+              },
+              app.translator.trans('fof-polls.forum.modal.submit')
+            )}
+          </div>
+        </div>
+      </div>,
+    ];
+  }
+
+  displayOptions() {
+    return Object.keys(this.options).map((el, i) => (
+      <div className="Form-group">
+        <fieldset className="Poll-answer-input">
+          <input
+            className="FormControl"
+            type="text"
+            name={'answer' + (i + 1)}
+            bidi={this.options[i]}
+            placeholder={app.translator.trans('fof-polls.forum.modal.option_placeholder') + ' #' + (i + 1)}
+          />
+        </fieldset>
+        {i >= 2
+          ? Button.component({
+              type: 'button',
+              className: 'Button Button--warning PollModal--button',
+              icon: 'fas fa-minus',
+              onclick: i >= 2 ? this.removeOption.bind(this, i) : '',
+            })
+          : ''}
+      </div>
+    ));
+  }
+
+  addOption() {
+    const setting = app.data['fof-polls.options.max'];
+    const max = (setting && parseInt(setting)) || 11;
+
+    if (this.options.length < max) {
+      this.options.push(Stream(''));
+    } else {
+      alert(app.translator.trans('fof-polls.forum.modal.max'));
+    }
+  }
+
+  removeOption(option) {
+    this.options.splice(option, 1);
+  }
+
+  onsubmit(e) {
+    e.preventDefault();
+
+    const poll = {
+      question: this.question(),
+      endDate: this.endDate(),
+      publicPoll: this.publicPoll(),
+    };
+    const options = this.options.map((a) => a()).filter(Boolean);
+
+    if (this.question() === '') {
+      alert(app.translator.trans('fof-polls.forum.modal.include_question'));
+
+      return;
     }
 
-    addOption() {
-        const setting = app.data['fof-polls.options.max'];
-        const max = (setting && parseInt(setting)) || 11;
+    if (options.length < 2) {
+      alert(app.translator.trans('fof-polls.forum.modal.min'));
 
-        if (this.options.length < max) {
-            this.options.push(Stream(''));
-        } else {
-            alert(app.translator.trans('fof-polls.forum.modal.max'));
-        }
+      return;
     }
 
-    removeOption(option) {
-        this.options.splice(option, 1);
-    }
+    poll.relationships = { options };
 
-    onsubmit(e) {
-        e.preventDefault();
+    this.attrs.onsubmit(poll);
 
-        const poll = {
-            question: this.question(),
-            endDate: this.endDate(),
-            publicPoll: this.publicPoll(),
-        };
-        const options = this.options.map((a) => a()).filter(Boolean);
-
-        if (this.question() === '') {
-            alert(app.translator.trans('fof-polls.forum.modal.include_question'));
-
-            return;
-        }
-
-        if (options.length < 2) {
-            alert(app.translator.trans('fof-polls.forum.modal.min'));
-
-            return;
-        }
-
-        poll.relationships = { options };
-
-        this.attrs.onsubmit(poll);
-
-        app.modal.close();
-    }
+    app.modal.close();
+  }
 }
--- js/src/forum/components/EditPollModal.js
+++ js/src/forum/components/EditPollModal.js
@@ -4,93 +4,94 @@ import Button from 'flarum/common/components/Button';
 import Stream from 'flarum/common/utils/Stream';
 import CreatePollModal from './CreatePollModal';
 
+/*global dayjs*/
 export default class EditPollModal extends CreatePollModal {
-    oninit(vnode) {
-        super.oninit(vnode);
+  oninit(vnode) {
+    super.oninit(vnode);
 
-        this.poll = this.attrs.poll;
+    this.poll = this.attrs.poll;
 
-        this.options = this.poll.options();
-        this.optionAnswers = this.options.map((o) => Stream(o.answer()));
-        this.question = Stream(this.poll.question());
-        this.endDate = Stream(this.poll.endDate());
-        this.publicPoll = Stream(this.poll.publicPoll());
-    }
+    this.options = this.poll.options();
+    this.optionAnswers = this.options.map((o) => Stream(o.answer()));
+    this.question = Stream(this.poll.question());
+    this.endDate = Stream(this.poll.endDate() && dayjs(this.poll.endDate()).format(this.dateTimeFormat));
+    this.publicPoll = Stream(this.poll.publicPoll());
+  }
 
-    title() {
-        return app.translator.trans('fof-polls.forum.modal.edit_title');
-    }
+  title() {
+    return app.translator.trans('fof-polls.forum.modal.edit_title');
+  }
 
-    displayOptions() {
-        return this.options.map((opt, i) => (
-            <div className="Form-group">
-                <fieldset className="Poll-answer-input">
-                    <input
-                        className="FormControl"
-                        type="text"
-                        name={'answer' + (i + 1)}
-                        bidi={this.optionAnswers[i]}
-                        placeholder={app.translator.trans('fof-polls.forum.modal.option_placeholder') + ' #' + (i + 1)}
-                    />
-                </fieldset>
+  displayOptions() {
+    return this.options.map((opt, i) => (
+      <div className="Form-group">
+        <fieldset className="Poll-answer-input">
+          <input
+            className="FormControl"
+            type="text"
+            name={'answer' + (i + 1)}
+            bidi={this.optionAnswers[i]}
+            placeholder={app.translator.trans('fof-polls.forum.modal.option_placeholder') + ' #' + (i + 1)}
+          />
+        </fieldset>
 
-                {i >= 2
-                    ? Button.component({
-                          type: 'button',
-                          className: 'Button PollModal--button',
-                          icon: 'fas fa-minus',
-                          onclick: i >= 2 ? this.removeOption.bind(this, i) : '',
-                      })
-                    : ''}
-            </div>
-        ));
-    }
-
-    addOption() {
-        const setting = app.data['fof-polls.options.max'];
-        const max = (setting && parseInt(setting)) || 11;
-
-        if (this.options.length < max) {
-            this.options.push(app.store.createRecord('poll_options'));
-            this.optionAnswers.push(Stream(''));
-        } else {
-            alert(app.translator.trans('fof-polls.forum.modal.max'));
-        }
-    }
-
-    removeOption(i) {
-        this.options.splice(i, 1);
-        this.optionAnswers.splice(i, 1);
-    }
-
-    onsubmit(e) {
-        e.preventDefault();
-
-        if (this.loading) return;
-
-        this.loading = true;
-
-        const options = this.options.map((o, i) => {
-            if (!o.data.attributes) o.data.attributes = {};
-
-            o.data.attributes.answer = this.optionAnswers[i]();
-
-            return o.data;
-        });
-
-        return this.poll
-            .save({
-                question: this.question(),
-                endDate: this.endDate() || false,
-                publicPoll: this.publicPoll(),
-                options,
+        {i >= 2
+          ? Button.component({
+              type: 'button',
+              className: 'Button PollModal--button',
+              icon: 'fas fa-minus',
+              onclick: i >= 2 ? this.removeOption.bind(this, i) : '',
             })
-            .then(() => {
-                document.location.reload();
-            })
-            .catch((e) => {
-                this.loaded();
-                this.onerror(e);
-            });
+          : ''}
+      </div>
+    ));
+  }
+
+  addOption() {
+    const setting = app.data['fof-polls.options.max'];
+    const max = (setting && parseInt(setting)) || 11;
+
+    if (this.options.length < max) {
+      this.options.push(app.store.createRecord('poll_options'));
+      this.optionAnswers.push(Stream(''));
+    } else {
+      alert(app.translator.trans('fof-polls.forum.modal.max'));
     }
+  }
+
+  removeOption(i) {
+    this.options.splice(i, 1);
+    this.optionAnswers.splice(i, 1);
+  }
+
+  onsubmit(e) {
+    e.preventDefault();
+
+    if (this.loading) return;
+
+    this.loading = true;
+
+    const options = this.options.map((o, i) => {
+      if (!o.data.attributes) o.data.attributes = {};
+
+      o.data.attributes.answer = this.optionAnswers[i]();
+
+      return o.data;
+    });
+
+    return this.poll
+      .save({
+        question: this.question(),
+        endDate: this.endDate() || false,
+        publicPoll: this.publicPoll(),
+        options,
+      })
+      .then(() => {
+        document.location.reload();
+      })
+      .catch((e) => {
+        this.loaded();
+        this.onerror(e);
+      });
+  }
 }
