60632230526ea248a17d006082ad6145b3a4aa62
Davide Iadeluca <davide.iadeluca@glowingblue.com>
2024-01-29 15:52:40 +0100

    refactor: break down logic

--- js/src/forum/components/Poll/PollOption.tsx
+++ js/src/forum/components/Poll/PollOption.tsx
@@ -2,19 +2,13 @@ import * as Mithril from 'mithril';
 import Component from 'flarum/common/Component';
 import PollOptionLabel from './PollOptionLabel';
 import PollOptionDescription from './PollOptionDescription';
+import PollOptionInput from './PollOptionInput';
 
 export default class PollOption extends Component {
   view(): Mithril.Children {
     return (
       <label className="PollOption-tmp">
-        <input
-          type="radio"
-          name="privacy-setting"
-          value="Private to Project Members"
-          className="PollOption-input"
-          aria-labelledby="privacy-setting-1-label"
-          aria-describedby="privacy-setting-1-description"
-        />
+        <PollOptionInput id={1} isResult={false} name="privacy-setting" value="Private to Project Members Nice" />
         <span className="PollOption-information">
           <PollOptionLabel text="Poll Option Label" />
           <PollOptionDescription text="Poll Option Description" />
--- js/src/forum/components/Poll/PollResult.tsx
+++ js/src/forum/components/Poll/PollResult.tsx
@@ -1,27 +1,21 @@
 import * as Mithril from 'mithril';
 import Component from 'flarum/common/Component';
 import PollOptionLabel from './PollOptionLabel';
+import PollResultsNumber from './PollResultNumber';
+import PollOptionInput from './PollOptionInput';
 
 export default class PollResults extends Component {
   view(): Mithril.Children {
     return (
       <label className="PollResult">
-        <input
-          type="radio"
-          name="privacy-setting"
-          value="Private to Project Members"
-          className="PollOption-input"
-          style="opacity: 0;"
-          aria-labelledby="privacy-setting-1-label"
-          aria-describedby="privacy-setting-1-description"
-        />
+        <PollOptionInput id={1} isResult={true} name="privacy-setting" value="Private to Project Members Nice" />
         <span className="PollResult-information">
           <div className="PollResults-row">
             <PollOptionLabel text="Poll Option Label" />
-            <span className="PollResult-number">76 %</span>
+            <PollResultsNumber number={64} />
           </div>
 
-          <input type="range" min="0" max="100" value="76" step="1" className="PollResult-input" />
+          <input type="range" min="0" max="100" value="64" step="1" className="PollResult-input" />
         </span>
       </label>
     );
