18e5ddc6922ed8297ab24556da9d3f9ca04b1d3e
IanM <ian@morland.me>
2024-03-06 16:00:25 +0000

    add pollimage interface

--- js/src/forum/components/Poll/PollImage.tsx
+++ js/src/forum/components/Poll/PollImage.tsx
@@ -1,7 +1,11 @@
 import type Mithril from 'mithril';
-import Component from 'flarum/common/Component';
+import Component, { ComponentAttrs } from 'flarum/common/Component';
 
-export default class PollImage extends Component {
+export interface PollImageAttrs extends ComponentAttrs {
+  imageUrl: string;
+}
+
+export default class PollImage extends Component<PollImageAttrs> {
   view(): Mithril.Children {
     const imageUrl = this.attrs.imageUrl;
 
