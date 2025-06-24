c63f574271dcb10029767fb99465826735f8afe1
Gianni Guida <gianni.guida@glowingblue.com>
2025-06-24 09:57:28 +0200

    feat: add controls dropdown to PollGroupListItem and remove meta infos

--- js/src/forum/components/PollGroup/PollGroupListItem.tsx
+++ js/src/forum/components/PollGroup/PollGroupListItem.tsx
@@ -1,10 +1,14 @@
 import Component, { ComponentAttrs } from 'flarum/common/Component';
 import PollGroup from '../../models/PollGroup';
 import classList from 'flarum/common/utils/classList';
-import ItemList from 'flarum/common/utils/ItemList';
 import app from 'flarum/forum/app';
 import PollListState from '../../states/PollListState';
 import PollListItem from '../Poll/PollListItem';
+import Button from 'flarum/common/components/LinkButton';
+import LogInModal from 'flarum/forum/components/LogInModal';
+import Mithril from 'mithril';
+import Dropdown from 'flarum/common/components/Dropdown';
+import PollGroupControls from '../../utils/PollGroupControls';
 
 interface PollGroupListItemAttrs extends ComponentAttrs {
   pollGroup: PollGroup;
@@ -12,7 +16,6 @@ interface PollGroupListItemAttrs extends ComponentAttrs {
 }
 
 export default class PollGroupListItem extends Component<PollGroupListItemAttrs, PollListState> {
-
   oninit(vnode: any) {
     super.oninit(vnode);
 
@@ -25,54 +28,46 @@ export default class PollGroupListItem extends Component<PollGroupListItemAttrs,
 
   view() {
     const pollGroup = this.attrs.pollGroup;
+    // @ts-expect-error
+    const controls = PollGroupControls.controls(pollGroup, this);
 
     return (
       <div className={classList('PollGroupListItem', 'PollGroupListItem--pollgroup')}>
         <div className="PollGroupListItem-main">
-          <h3 className="PollGroupListItem-title">
-            {pollGroup.name()}
-          </h3>
-          <ul className="PollGroupListItem-meta">
-            {this.metaItems().toArray()}
-          </ul>
-          <p className="PollGroupListItem-excerpt"></p>
+          <h3 className="PollGroupListItem-title">{pollGroup.name()}</h3>
+          {this.controlsView(controls.toArray())}
         </div>
 
-        {this.state.getPages().length > 0 ? (
-          <ul className="PollGroupListItem-polls">
-            {this.state.getPages().map((pg) => {
+        <ul className="PollGroupListItem-polls">
+          {this.state.getPages().length > 0 ? (
+            this.state.getPages().map((pg) => {
               return pg.items.map((poll) => (
                 <li key={poll.id()} data-id={poll.id()}>
                   <PollListItem poll={poll} params={{}} />
                 </li>
               ));
-            })}
-          </ul>
-        ) : (
-          <p>{app.translator.trans('fof-polls.forum.pollgroups_list.no_polls')}</p>
-        )}
+            })
+          ) : (
+            <span>{app.translator.trans('fof-polls.forum.poll_groups.list_page.no_polls')}</span>
+          )}
+        </ul>
       </div>
     );
   }
 
-  metaItems(): ItemList<any> {
-    const items = new ItemList();
-    const pollGroup = this.attrs.pollGroup;
-
-    items.add(
-      'user',
-      app.translator.trans('fof-polls.forum.pollgroups_list.created_by', {
-        user: pollGroup.user(),
-      })
+  controlsView(controls: Mithril.ChildArray): Mithril.Children {
+    return (
+      !!controls.length && (
+        <Dropdown
+          icon="fas fa-ellipsis-v"
+          className="PollGroupListItem-controls"
+          menuClassName="Dropdown-menu--right"
+          buttonClassName="Button Button--icon Button--flat"
+          accessibleToggleLabel={app.translator.trans('fof-polls.forum.poll_controls.toggle_dropdown_accessible_label')}
+        >
+          {controls}
+        </Dropdown>
+      )
     );
-
-    items.add(
-      'date',
-      app.translator.trans('fof-polls.forum.pollgroups_list.created_at', {
-        ago: dayjs(pollGroup.createdAt()).fromNow(),
-      })
-    );
-
-    return items;
   }
 }
