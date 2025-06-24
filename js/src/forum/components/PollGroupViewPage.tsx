df9510bb13fe5f148ac84c7121a1222087bb070a
Gianni Guida <gianni.guida@glowingblue.com>
2025-06-24 20:20:34 +0200

    refactor: remove unused imports in PollGroupViewPage component

--- js/src/forum/components/PollGroupViewPage.tsx
+++ js/src/forum/components/PollGroupViewPage.tsx
@@ -2,14 +2,7 @@ import app from 'flarum/forum/app';
 import Mithril from 'mithril';
 import PollGroup from '../models/PollGroup';
 import PollGroupListItem from './PollGroup/PollGroupListItem';
-import IndexPageHero from './PollPageHero';
-import LoadingIndicator from 'flarum/common/components/LoadingIndicator';
 import ItemList from 'flarum/common/utils/ItemList';
-import IndexPage from 'flarum/forum/components/IndexPage';
-import SelectDropdown from 'flarum/common/components/SelectDropdown';
-import listItems from 'flarum/common/helpers/listItems';
-import Button from 'flarum/common/components/Button';
-import LogInModal from 'flarum/forum/components/LogInModal';
 import { AbstractPollGroupsPage } from './AbstractPollGroupsPage';
 
 export default class PollGroupViewPage extends AbstractPollGroupsPage {
