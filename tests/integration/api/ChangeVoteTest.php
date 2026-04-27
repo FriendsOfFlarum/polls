a5c662937d9a368413481ca57eddda4303dc15cf
Grimur Vid Neyst <grimur.vid-neyst@glowingblue.com>
2026-04-27 17:24:38 +0200

    fix: global poll permission issue

--- tests/integration/api/ChangeVoteTest.php
+++ tests/integration/api/ChangeVoteTest.php
@@ -36,6 +36,7 @@ class ChangeVoteTest extends TestCase
                 $this->normalUser(),
                 ['id' => 3, 'username' => 'polluser', 'email' => 'polluser@machine.local', 'password' => 'too-obscure', 'is_email_confirmed' => true],
                 ['id' => 4, 'username' => 'moderator', 'email' => 'moderator@machine.local', 'password' => 'too-obscure', 'is_email_confirmed' => true],
+                ['id' => 5, 'username' => 'noperms', 'email' => 'noperms@machine.local', 'password' => 'too-obscure', 'is_email_confirmed' => false],
             ],
             Discussion::class => [
                 ['id' => 1, 'title' => 'Discussion 1', 'comment_count' => 1, 'participant_count' => 1, 'created_at' => '2021-01-01 00:00:00'],
@@ -57,8 +58,10 @@ class ChangeVoteTest extends TestCase
                 ['user_id' => 4, 'group_id' => 4],
             ],
             'group_permission' => [
+                ['permission' => 'discussion.polls.vote', 'group_id' => 3],
                 ['permission' => 'discussion.polls.start', 'group_id' => 4],
                 ['permission' => 'startGlobalPoll', 'group_id' => 4],
+                ['permission' => 'discussion.polls.vote', 'group_id' => 4],
                 ['permission' => 'uploadPollImages', 'group_id' => 4],
                 ['permission' => 'polls.changeVote', 'group_id' => 4],
             ],
@@ -230,5 +233,25 @@ class ChangeVoteTest extends TestCase
         // Check that options have voteCount attribute
         $firstOption = reset($optionTypes);
         $this->assertArrayHasKey('voteCount', $firstOption['attributes']);
+    }
+
+    #[Test]
+    public function user_without_vote_permission_cannot_vote_on_global_poll()
+    {
+        $response = $this->send(
+            $this->request('PATCH', '/api/polls/2/votes', [
+                'authenticatedAs' => 5,
+                'json'            => [
+                    'data' => [
+                        'optionIds' => [
+                            3,
+                        ],
+                    ],
+                ],
+            ])
+        );
+
+        $this->assertEquals(403, $response->getStatusCode());
+        $this->assertFalse(PollVote::where('user_id', 5)->where('poll_id', 2)->exists());
     }
 }
